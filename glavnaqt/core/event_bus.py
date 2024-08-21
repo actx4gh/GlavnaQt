dc0a2dfa3097811d5fe72fc8ee27bff800eb1bfb
Aaron Colichia <actx4gh@gb4emlsep.mailer.me>
2024-08-21 18:46:17 +0100

    event bus, status bar work

---
+++ glavnaqt/core/event_bus.py
@@ -0,0 +1,15 @@
+# glavnaqt/core/event_bus.py
+
+class EventBus:
+    def __init__(self):
+        self.listeners = {}
+
+    def subscribe(self, event_type, callback):
+        if event_type not in self.listeners:
+            self.listeners[event_type] = []
+        self.listeners[event_type].append(callback)
+
+    def emit(self, event_type, *args, **kwargs):
+        if event_type in self.listeners:
+            for callback in self.listeners[event_type]:
+                callback(*args, **kwargs)
